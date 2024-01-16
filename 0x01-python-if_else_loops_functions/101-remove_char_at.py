15bcd756ece29545928718a22ac2dc5b4813836e
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-01-16 23:36:40 +0300

    Task 15

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
     if n < 0 or n >= len(str):
-    # If index is out of bounds, return the original string
+        # If index is out of bounds, return the original string
         return (str)
     # Construct a new string excluding the character at index n
     return(str[:n] + str[n + 1:])
