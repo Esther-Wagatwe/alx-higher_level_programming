31499b05522c64879a1e7d3bbd3ed004009c0f7e
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-01-18 16:37:30 +0300

    Task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,6 @@ if __name__ == "__main__":
     import hidden_4
     names = dir(hidden_4)
     for i in names:
-        if i[:2] != "__": #names starting with double underscores are considered private in python
+        # in python names starting with double underscores are private
+        if i[:2] != "__":
             print(i)
