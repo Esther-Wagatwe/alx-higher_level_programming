7d0a984472f593c5eb5f0df7d6eeb044f73bfa0a
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-02-22 18:24:14 +0300

    Creates a method that  returns the JSON string representation of list_dictionaries

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Defines a class that will be the base of all other classes."""
+import json
 
 
 class Base:
@@ -13,3 +14,19 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Serializes list_dictionaries.
+
+        Args:
+            list_dictionaries (object): a list of dictionaries.
+
+        returns:
+            the JSON string representation of list_dictionaries
+        """
+        if list_dictionaries is None or not list_dictionaries:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
