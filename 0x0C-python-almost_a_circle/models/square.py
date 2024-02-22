05932a55845b1c0193862231861085e516ac0e87
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-02-22 16:48:42 +0300

    Creates a getter and setter size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -17,3 +17,13 @@ class Square(Rectangle):
             """
         return "[{}] ({}) {}/{} - {}".\
             format(type(self).__name__, self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        """Get/ set the value of size."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
