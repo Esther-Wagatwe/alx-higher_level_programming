3bf94b4768a3f4b50f99e36a6f7e8d8fb16f4925
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-02-22 13:49:54 +0300

    Creates validate_integer method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -8,14 +8,6 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """
         Class representing a rectangle
-
-        Attributes:
-            width (int): The width of the rectangle.
-            height (int): The height of the rectangle.
-            x (int): The x-coordinate of the rectangle's position.
-            y (int): The y-coordinate of the rectangle's position.
-            id (int): The identifier of the rectangle.
-
         """
         super().__init__(id)
         self.width = width
@@ -23,22 +15,53 @@ class Rectangle(Base):
         self.x = x
         self.y = y
 
-        @property
-        def width(self):
-            """Get the width of the rectangle."""
-            return self.__width
+    @property
+    def width(self):
+        """Get the width of the rectangle."""
+        return self.__width
 
-        @width.setter
-        def width(self, x):
-            """Set the width of the rectangle."""
-            self.__width = self.__x
+    @width.setter
+    def width(self, value):
+        """Set the width of the rectangle."""
+        self.validate_int("width", value, False)
+        self.__width = value
 
-        @property
-        def height(self):
-            """Set the height of the rectangle."""
-            return self.__height
+    @property
+    def height(self):
+        """Set the height of the rectangle."""
+        return self.__height
 
-        @height.setter
-        def height(self, y):
-            """Get the height of the rectangle."""
-            self.__height = self.__y
+    @height.setter
+    def height(self, value):
+        """Get the height of the rectangle."""
+        self.validate_int("height", value, False)
+        self.__height = value
+
+    @property
+    def x(self):
+        """Get/set x coordinate of the rectangle."""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.validate_int("x", value)
+        self.__x = value
+
+    @property
+    def y(self):
+        """Get/set y coordinate of the rectangle."""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.validate_int("y", value)
+        self.__y = value
+
+    def validate_int(self, attribute, value, eq=True):
+        """Method to validate all setter methods and instantiation."""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(attribute))
+        if eq and value < 0:
+            raise ValueError("{} must be >= 0".format(attribute))
+        elif not eq and value <= 0:
+            raise ValueError("{} must be > 0".format(attribute))
