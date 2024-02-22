911d4a2031db0b73a848cb81da85d782854f1d31
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-02-22 20:35:32 +0300

    Creates a static method that returns the list of the JSON string representation

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,9 +24,40 @@ class Base:
             list_dictionaries (object): a list of dictionaries.
 
         returns:
-            the JSON string representation of list_dictionaries
+            the JSON string representation of list_dictionaries.
         """
         if list_dictionaries is None or not list_dictionaries:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        Method that writes the JSON string repr of list_objs to a file.
+        Args:
+            cls (cls): The name of the class
+            list_objs (list): list of instances who inherits of Base.
+        """
+        filename = cls.__name__ + ".json"
+        with open(filename, "w", encoding="utf-8") as jsonfile:
+            if list_objs is None:
+                jsonfile.write("[]")
+            else:
+                list_dicts = [i.to_dictionary() for i in list_objs]
+                jsonfile.write(cls.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        Static method that deserializes json_string.
+
+        Args:
+            json_string (str): string representing a list of dictionaries.
+        Return:
+            the list represented by json_string.
+        """
+        if json_string is None or not json_string:
+            return []
+        else:
+            return json.loads(json_string)
