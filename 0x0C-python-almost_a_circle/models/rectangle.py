915e924a0fa903ab43dd7a1061fda0206a92a4c1
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-02-22 15:43:34 +0300

    create update module that assigns an argument to each attribute

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -84,3 +84,16 @@ class Rectangle(Base):
         """Returns string info about this rectangle."""
         return "[Rectangle] ({}) {}/{} - {}/{}".\
             format(self.id, self.x, self.y, self.width, self.height)
+
+    def update(self, *args):
+        """Assigns arguments to attributes: id, width, height, x, y."""
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.width = args[1]
+        if len(args) > 2:
+            self.height = args[2]
+        if len(args) > 3:
+            self.x = args[3]
+        if len(args) > 4:
+            self.y = args[4]
