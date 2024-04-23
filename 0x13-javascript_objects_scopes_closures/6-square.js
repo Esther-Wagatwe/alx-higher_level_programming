765026fec1f445a6eb5da831fd1d228772a5ad69
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-04-23 23:23:43 +0300

    Square #1

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const prevSquare = require('./5-square.js');
+
+class Square extends prevSquare {
+  charPrint (c) {
+    for (let i = 0; i < this.height; i++) {
+      if (c === undefined) {
+        console.log('X'.repeat(this.width));
+      } else {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+}
+
+module.exports = Square;
