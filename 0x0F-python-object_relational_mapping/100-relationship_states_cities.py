da86fafa50db5c91a4b39e6710e50a9833c36308
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-04-27 19:27:00 +0300

    City relationship

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -5,8 +5,8 @@
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
-from relationship_city import City
+from relationship_state import State
+from relationship_city import City, Base
 
 
 if __name__ == "__main__":
