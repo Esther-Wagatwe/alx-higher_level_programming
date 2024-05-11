04172f4ab8c569092c253ea1ecb8a88e0979a86b
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-05-11 03:23:04 +0300

    What's my status? #1

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Fetches https://alx-intranet.hbtn.io/status
+must use the package requests
+"""
+import requests
+
+
+if __name__ == "__main__":
+    """Perform a get operation."""
+    url =  "https://alx-intranet.hbtn.io/status"
+    response = requests.get(url)
+    print("Body response:")
+    print("\t- type: {}".format(response.text.__class__))
+    print("\t- content: {}".format(response.text))
