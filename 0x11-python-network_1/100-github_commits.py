1914d07de3b29fb2fdaee5246193a13dfcf216e4
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-05-11 06:15:57 +0300

    Interview

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -11,15 +11,15 @@ if __name__ == "__main__":
     """Perform a GET operation."""
     repo_name = sys.argv[1]
     owner_name = sys.argv[2]
-    url = "https://api.github.com/repos/{}/{}".format(owner_name, repo_name)
+    url = "https://api.github.com/repos/{}/{}/commits".format(owner_name, repo_name)
     response = requests.get(url)
     response_json = response.json()
 
     try:
         for i in range(10):
             print("{}: {}".format(
-                  response_json[i].get("sha"),
-                  response_json[i].get("commit").get("author").get("name")))
+                  response_json[i].get('sha'),
+                  response_json[i].get('commit').get('author').get('name')))
 
     except IndexError:
         pass
