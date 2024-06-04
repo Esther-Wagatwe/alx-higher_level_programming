473537623d4c69ee92dcbbb17d02186f9514b6ed
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-06-04 21:49:10 +0300

    debugging

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -5,16 +5,15 @@ request(url, function (error, response, body) {
   if (error) {
     console.log(error);
   }
-  const tasksCompleted = {};
   const tasks = JSON.parse(body);
+  const tasksCompleted = {};
   // Iterate over the tasks and count the completed tasks for each user
-  tasks.forEach(task => {
+  tasks.forEach((task) => {
     if (task.completed) {
-      const userId = task.userId.toString();
-      if (tasksCompleted[userId]) {
-        tasksCompleted[userId] += 1;
+      if (tasksCompleted[task.userId]) {
+        tasksCompleted[task.userId] += 1;
       } else {
-        tasksCompleted[userId] = 1;
+        tasksCompleted[task.userId] = 1;
       }
     }
   });
